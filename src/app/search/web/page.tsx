2778b2228edafe8dd6bc639b0374465e34124e51
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-03-26 19:33:27 +0300

    work on search functionality with google api

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Link from "next/link";
+const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+const GOOGLE_CX_ID = process.env.NEXT_PUBLIC_GOOGLE_CONTEXT_ID;
 
-const SearchPage = () => {
+const SearchPage = async({searchParams:{q}}:{searchParams:{q:string}}) => {
+
+	const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX_ID}&q=${q}`)
+	const data = await response.json();
+	const result = await data.items;
+
 	return (
-		<div>
-			Search Page
-		</div>
+		<>
+			{result && result.map((m:any,index:number)=>(
+				<div key={index}>
+					<h1>{m.title}</h1>
+					<Link href={m.link}>{m.displayLink}</Link>
+				</div>
+			))}
+		</>
 	);
 };
 
